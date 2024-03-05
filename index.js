bf6b688b5a9f6c118803561aa0ad1ca2b46d796c
abuXahra <abdulmuminisah@yahoo.com>
2024-03-05 11:01:20 +0100

    updated

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 5000;
 
 
 const corsOptions = {
-    origin: ["http://localhost:3000", "https://blog-9erg.onrender.com"], //https://pblog-lno1.onrender.com
+    origin: ["http://localhost:3000", "https://newsblog-244u.onrender.com"], //https://pblog-lno1.onrender.com
     // methods: ["POST", "GET"],
     credentials: true
 };
