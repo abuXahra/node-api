edb4e8baca17336b047ae3c80eb11345e015d75c
abuXahra <abdulmuminisah@yahoo.com>
2024-03-05 10:38:56 +0100

    updated

--- routes/category.js
+++ routes/category.js
@@ -83,4 +83,49 @@ router.get('/:categoryId/posts', async (req, res) => {
 });
 
 
+// with pagination
+router.get('/:categoryId/post', async (req, res) => {
+
+    try {
+        const categoryId = req.params.categoryId;
+
+        let query = Post.find({ categories: categoryId }).sort({ createdAt: -1 });
+
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = parseInt(req.query.limit) || 4;
+        const skip = (page - 1) * pageSize;
+        const total = await Post.countDocuments();
+
+        const pages = Math.ceil(total / pageSize);
+
+        query = query.skip(skip).limit(pageSize);
+
+        if (page > pages) {
+            return res.status(404).json({
+                status: "fail",
+                message: "No page found",
+            });
+        }
+
+        const result = await query;
+
+        res.status(200).json({
+            status: "success",
+            count: result.length,
+            page,
+            pages,
+            data: result,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: "error",
+            message: "Server Error",
+        });
+    }
+});
+
+
+
+
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -148,17 +148,66 @@ router.get('/:id', async (req, res) => {
 })
 
 
-//GET USER POSTS 
-router.get("/user/:userId", async (req, res) => {
+//GET USER POSTS without pagination
+// router.get("/user/:userId", async (req, res) => {
+//     try {
+//         const posts = await Post.find({ userId: req.params.userId })
+//             .populate('categories')
+//             .populate('comments').sort({ createdAt: -1 })
+//         res.status(200).json(posts)
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// })
+
+
+//GET USER POSTS with pagination
+router.get('/user/:userId', async (req, res) => {
+
     try {
-        const posts = await Post.find({ userId: req.params.userId })
+        const userId = req.params.userId;
+
+        let query = Post.find({ userId: userId })
             .populate('categories')
-            .populate('comments').sort({ createdAt: -1 })
-        res.status(200).json(posts)
-    } catch (err) {
-        res.status(500).json(err);
+            .populate('comments')
+            .sort({ createdAt: -1 });
+
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = parseInt(req.query.limit) || 4;
+        const skip = (page - 1) * pageSize;
+        const total = await Post.countDocuments();
+
+        const pages = Math.ceil(total / pageSize);
+
+        query = query.skip(skip).limit(pageSize);
+
+        if (page > pages) {
+            return res.status(404).json({
+                status: "fail",
+                message: "No page found",
+            });
+        }
+
+        const result = await query;
+
+        res.status(200).json({
+            status: "success",
+            count: result.length,
+            page,
+            pages,
+            data: result,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: "error",
+            message: "Server Error",
+        });
     }
-})
+});
+
+
+
 
 
 // GET POST AND ITS CATEGORIES & COMMENTS
